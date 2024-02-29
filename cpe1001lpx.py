19983e97ed0c2f33242170d5da94a0be5e288fa2
William <wconner09@gmail.com>
2024-02-28 21:42:37 -0500

    File importion finished

--- cpe1001lpx.py
+++ cpe1001lpx.py
@@ -1,2 +1,13 @@
-print ("Goodbye")
-#you have to run code before sending to github
+import sys
+
+file_name = sys.argv[1]
+
+file1 = open(file_name, 'r')
+Lines = file1.readlines()
+
+count = 0   
+for line in Lines:
+    count += 1
+    print("Line {}: {}".format(count, line.strip()))
+
+print(file_name)
