c6054eb6199defaa50b3bdd98fef6c808e514aa2
Will Conner <wconner09@gmail.com>
2024-02-29 20:37:09 -0500

    changes have been made

--- cpe1001lpx.py
+++ cpe1001lpx.py
@@ -1,5 +1,8 @@
 import sys
 
+def parse_line(line):
+    operands = line.split('+')
+
 file_name = sys.argv[1]
 
 file1 = open(file_name, 'r')
@@ -8,6 +11,11 @@ Lines = file1.readlines()
 count = 0   
 for line in Lines:
     count += 1
+    line = line.strip()
     print("Line {}: {}".format(count, line.strip()))
+    result = parse_line(line)
+    print("   Result: {}".format(result))
+    total_sum += result
 
-print(file_name)
+print("Total sum of valid additions im the file", total_sum)
+
