35aa0421c611ee139113cde2e0722a59e223f681
Will Conner <wconner09@gmail.com>
2024-02-29 23:21:07 -0500

    finished parse

--- cpe1001lpx.py
+++ cpe1001lpx.py
@@ -1,4 +1,5 @@
 import sys
+import string
 
 file_name = sys.argv[1]
 
@@ -9,4 +10,12 @@ count = 0
 for line in lines:
     count += 1
     line = line.strip()
-    print("Line {}: {}".format(count, line.strip()))
+    print("Line {}: {}".format(count, line))
+    
+    words = line.split()
+
+    cleaned_words = []
+    for word in words:
+        cleaned_word = word.translate(str.maketrans('', '', string.punctuation))
+        cleaned_words.append(cleaned_word)
+    
