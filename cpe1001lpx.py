c6d2aed0f29be7ec7a84b7c76a5c29ae81709003
Will Conner <wconner09@gmail.com>
2024-03-01 00:17:57 -0500

    kill me pld

--- cpe1001lpx.py
+++ cpe1001lpx.py
@@ -26,9 +26,7 @@ for line in lines:
 
     cleaned_words = []
     for word in words:
-        # Check if the word is a whole number
         if word.isdigit():
-            # If it's a whole number, define it as itself
             cleaned_words.append(word)
         else:
             cleaned_word = word.translate(str.maketrans('', '', string.punctuation))
@@ -36,11 +34,9 @@ for line in lines:
 
     print("Parsed words with definitions:")
     for cleaned_word in cleaned_words:
-        # Check if the word has a definition
         if cleaned_word in word_definitions:
             print(cleaned_word, "-", word_definitions[cleaned_word])
         else:
-            # If the word is a whole number, define it as itself
             if cleaned_word.isdigit():
                 print(cleaned_word, "-", cleaned_word)
             else:
