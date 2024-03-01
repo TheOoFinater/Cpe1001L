dbbeedf46be6ed1fbd9309eb5cbe4371b1c24a05
Will Conner <wconner09@gmail.com>
2024-02-29 20:37:29 -0500

    changes

--- cpe1001lpx.py
+++ cpe1001lpx.py
@@ -1,21 +1,16 @@
 import sys
-
-def parse_line(line):
-    operands = line.split('+')
+from nltk.tokenize import word_tokenize
 
 file_name = sys.argv[1]
 
 file1 = open(file_name, 'r')
-Lines = file1.readlines()
+lines = file1.readlines()
 
 count = 0   
-for line in Lines:
+for line in lines:
     count += 1
     line = line.strip()
     print("Line {}: {}".format(count, line.strip()))
-    result = parse_line(line)
-    print("   Result: {}".format(result))
-    total_sum += result
-
-print("Total sum of valid additions im the file", total_sum)
-
+s = file1
+tokens = word_tokenize(s)
+print(tokens)
